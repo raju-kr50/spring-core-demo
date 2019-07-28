ad5266ad730617fa75393ca4dd6e07b3c914f2f0
raju.kumar <raju.kumar@cgi.com>
2019-07-28 23:43:39 +0530

    Task2 completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -11,6 +11,7 @@ import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
+        //*********************************************Task 1***********************************************************
         //Using ApplicationContext
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Actor actor = context.getBean("actor",Actor.class);
@@ -23,6 +24,26 @@ public class Main {
         BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
         Movie movie1= factory.getBean("movie", Movie.class);
         System.out.println(movie);
+        System.out.println("*****************************************************************");
+
+        //**********************************************Task 2**********************************************************
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+        Actor actor1 = context1.getBean("actor1", Actor.class);
+        System.out.println(actor1);
+
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("beans.xml");
+        Actor actor2 = context2.getBean("actor2", Actor.class);
+        System.out.println(actor2);
+
+        ApplicationContext context3 = new ClassPathXmlApplicationContext("beans.xml");
+        Actor actor3 = context3.getBean("actor3", Actor.class);
+        System.out.println(actor3);
+
+        ApplicationContext context4 = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie2 = context4.getBean("movie1", Movie.class);
+        System.out.println(movie2);
+
+        System.out.println(movie==movie2);
 
     }
 }
